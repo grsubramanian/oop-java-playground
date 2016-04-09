4b488127c2bc19a23d85cc483bf9ff7654296ad7
gokul2411s <gokul2411s@gmail.com>
2016-04-10 01:01:38 +0530

    Fixed class javadoc typo in EventObserver.java

--- src/main/java/gokul2411s/projects/patternsrepo/events/EventObserver.java
+++ src/main/java/gokul2411s/projects/patternsrepo/events/EventObserver.java
@@ -1,7 +1,7 @@
 package gokul2411s.projects.patternsrepo.events;
 
 /**
- * Represents events interested in events of type {@link T}.
+ * Represents observers interested in events of type {@link T}.
  */
 public interface EventObserver<T> {
   void react(T event);
